0275d3cfa9541cf96640fbf273fd99f4d047b371
rendani123456 <127829971+rendani123456@users.noreply.github.com>
2024-05-19 17:47:57 +0700

    Web Portofolio

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+// NAVBAR FIXED
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+// HUMBERGER MENU
+
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
